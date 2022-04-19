7193a278b1a0a5d4f401ec018e1344418f679aee
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-04-19 11:22:55 +0200

    tourcard component without styling

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TourCard from './components/TourCard';
+
+function App() {
+  return (
+    <div className="App">
+      <TourCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TourCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Paper from '@mui/material/Paper'
+function TourCard() {
+  return (
+    <Paper elevation={3} >
+        TourCard
+    </Paper>
+  )
+}
+
+export default TourCard
